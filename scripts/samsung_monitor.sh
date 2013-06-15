63af3ea57934b621292294ae6c13dc8bb49e452a
kali <kali@futeisha.org>
2013-06-15 23:03:12 +0900

    add script for monitor mode

---
+++ scripts/samsung_monitor.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#cvt 1366 768
+xrandr --newmode "1368x768"   85.25  1368 1440 1576 1784  768 771 781 798 -hsync +vsync 
+
+#xrandr --addmode VGA-0 1368x768
+xrandr --addmode CRT1 1368x768
+
