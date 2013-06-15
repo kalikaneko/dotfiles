c7ef93c5418ed5badf4e979265a049866e17fe3c
kali <kali@futeisha.org>
2013-06-15 23:05:09 +0900

    add screenlayout folder for arandr

---
+++ screenlayout/home2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS --mode 1366x768 --pos 0x0 --rotate normal --output CRT1 --mode 1280x1024 --pos 1366x0 --rotate normal --output DFP1 --off
---
+++ screenlayout/home4.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS --mode 1366x768 --pos 0x0 --rotate normal --output CRT1 --mode 1368x768 --pos 1366x0 --rotate normal --output DFP1 --off
