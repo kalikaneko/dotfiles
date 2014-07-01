e10ceb6c05877e3e34bc7e49ac81434098e27ea9
Kali Kaneko <kali@futeisha.org>
2014-07-01 01:09:48 -0500

    arandr script

--- screenlayout/home_sweet_homie.sh
+++ screenlayout/home_sweet_homie.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS --mode 1280x768 --pos 0x0 --rotate normal --output CRT1 --mode 1280x800 --pos 1280x0 --rotate normal --output DFP1 --off
+xrandr --output VGA-0 --mode 1280x800 --pos 1280x0 --rotate normal --output LVDS-0 --mode 1280x800 --pos 0x0 --rotate normal --output DVI-D-0 --off
